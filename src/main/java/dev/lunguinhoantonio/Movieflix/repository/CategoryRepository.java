de6ce1c00ed91ce43ddf54195796505f195ca178
lunguinhoantonio <antoniolunguinho007@gmail.com>
2025-11-16 17:10:06 -0300

    feat: add Category repository

---
+++ src/main/java/dev/lunguinhoantonio/Movieflix/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package dev.lunguinhoantonio.Movieflix.repository;
+
+import dev.lunguinhoantonio.Movieflix.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
