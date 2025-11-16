c86c1cd75a077161b6745bacb428b9330c0743a4
lunguinhoantonio <antoniolunguinho007@gmail.com>
2025-11-16 18:34:29 -0300

    feat: add Category mapper

--- src/main/java/dev/lunguinhoantonio/Movieflix/mapper/CategoryMapper.java
+++ src/main/java/dev/lunguinhoantonio/Movieflix/mapper/CategoryMapper.java
@@ -1,4 +1,23 @@
 package dev.lunguinhoantonio.Movieflix.mapper;
 
+import dev.lunguinhoantonio.Movieflix.controller.request.CategoryRequest;
+import dev.lunguinhoantonio.Movieflix.controller.response.CategoryResponse;
+import dev.lunguinhoantonio.Movieflix.entity.Category;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class CategoryMapper {
+    public static Category toCategory(CategoryRequest categoryRequest) {
+        return Category
+                .builder()
+                .name(categoryRequest.name())
+                .build();
+    }
+
+    public static CategoryResponse toCategoryResponse(Category category) {
+        return CategoryResponse.builder()
+                .id(category.getId())
+                .name(category.getName())
+                .build();
+    }
 }
