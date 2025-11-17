5dfa334ddc5e105b9fdd1d27a1455c7429e3e4cf
lunguinhoantonio <antoniolunguinho007@gmail.com>
2025-11-16 21:39:20 -0300

    feat: add rest of service

--- src/main/java/dev/lunguinhoantonio/Movieflix/service/CategoryService.java
+++ src/main/java/dev/lunguinhoantonio/Movieflix/service/CategoryService.java
@@ -1,7 +1,31 @@
 package dev.lunguinhoantonio.Movieflix.service;
 
+import dev.lunguinhoantonio.Movieflix.entity.Category;
+import dev.lunguinhoantonio.Movieflix.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class CategoryService {
+    private final CategoryRepository repository;
+
+    public List<Category> findAll() {
+        return repository.findAll();
+    }
+
+    public Category save(Category category) {
+        return repository.save(category);
+    }
+
+    public Optional<Category> findById(Long id) {
+        return repository.findById(id);
+    }
+
+    public void deleteById(Long id) {
+        repository.deleteById(id);
+    }
 }
