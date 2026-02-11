60d679b0a935724cfe7286cf01a0f380603a1875
lunguinhoantonio <antoniolunguinho007@gmail.com>
2026-02-11 05:16:47 -0300

    feat: add encoder

--- src/main/java/dev/lunguinhoantonio/Movieflix/service/UserService.java
+++ src/main/java/dev/lunguinhoantonio/Movieflix/service/UserService.java
@@ -3,14 +3,18 @@ package dev.lunguinhoantonio.Movieflix.service;
 import dev.lunguinhoantonio.Movieflix.entity.User;
 import dev.lunguinhoantonio.Movieflix.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
 
     public User save(User user) {
+        String password = user.getPassword();
+        user.setPassword(passwordEncoder.encode(password));
         return repository.save(user);
     }
 }
