2ec34a0b01c2d76ef7039fa47030b33ea7648754
lunguinhoantonio <antoniolunguinho007@gmail.com>
2026-02-11 05:15:08 -0300

    feat: add UserDetails class and override his methods

--- src/main/java/dev/lunguinhoantonio/Movieflix/entity/User.java
+++ src/main/java/dev/lunguinhoantonio/Movieflix/entity/User.java
@@ -3,8 +3,12 @@ package dev.lunguinhoantonio.Movieflix.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 @Builder
 @Entity
@@ -13,7 +17,7 @@ import java.time.LocalDateTime;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -31,4 +35,33 @@ public class User {
     @Column(name = "created_at")
     private LocalDateTime createdAt;
 
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
 }
