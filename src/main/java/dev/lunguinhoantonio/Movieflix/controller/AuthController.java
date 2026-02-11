267c71f0c53f23b4f972b6b85838beb3bd4f81b0
lunguinhoantonio <antoniolunguinho007@gmail.com>
2026-02-11 05:12:04 -0300

    feat: add login request

--- src/main/java/dev/lunguinhoantonio/Movieflix/controller/AuthController.java
+++ src/main/java/dev/lunguinhoantonio/Movieflix/controller/AuthController.java
@@ -1,14 +1,19 @@
 package dev.lunguinhoantonio.Movieflix.controller;
 
+import dev.lunguinhoantonio.Movieflix.config.TokenService;
+import dev.lunguinhoantonio.Movieflix.controller.request.LoginRequest;
 import dev.lunguinhoantonio.Movieflix.controller.request.UserRequest;
+import dev.lunguinhoantonio.Movieflix.controller.response.LoginResponse;
 import dev.lunguinhoantonio.Movieflix.controller.response.UserResponse;
 import dev.lunguinhoantonio.Movieflix.entity.User;
 import dev.lunguinhoantonio.Movieflix.mapper.UserMapper;
-import dev.lunguinhoantonio.Movieflix.repository.UserRepository;
 import dev.lunguinhoantonio.Movieflix.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,10 +24,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
     private final UserService service;
+    private final AuthenticationManager authenticationManager;
+    private final TokenService tokenService;
 
     @PostMapping("/register")
     public ResponseEntity<UserResponse> registrar(@RequestBody UserRequest request) {
         User savedUser = service.save(UserMapper.toUser(request));
         return ResponseEntity.status(HttpStatus.CREATED).body(UserMapper.toUserResponse(savedUser));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
+        UsernamePasswordAuthenticationToken userAndPass = new UsernamePasswordAuthenticationToken(request.email(), request.password());
+        Authentication authenticate = authenticationManager.authenticate(userAndPass);
+
+        User user = (User) authenticate.getPrincipal();
+
+        String token = tokenService.generateToken(user);
+
+        return ResponseEntity.ok(new LoginResponse(token));
     }
 }
